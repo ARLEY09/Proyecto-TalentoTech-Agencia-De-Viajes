d533c28ea3a79c6ac176fceadc0ed268e4561898
Jesús Arley Lema <jesusarley1998@gmail.com>
2026-03-16 22:00:38 -0500

    Definiendo rutas

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express';
+import router from './routes/index.js'
 
 const app = express();
 
 
 //Definimos el puesto que vamos a usar
 const port = process.env.PORT || 4000;
+
+// habilitar pug (lo usamos para tamplates o vistas)
+app.set('view engine','pug')
+
+//Agregamos las rutas
+app.use('/',router)
 
 app.listen(() => {
     console.log(`El servidor esta funcionando en el puerto ${port}`)
